fa8aee618a50561ef585471af6d65a4ab09a869a
EunjinBang <twin1963@sookmyung.ac.kr>
2022-10-04 16:30:14 +0900

    01

--- main.c
+++ main.c
@@ -6,16 +6,16 @@
 int main(int argc, char *argv[]) {
 	
 	int i;
-	printf("정수 하나를 입력하시오 :\n");
-	scanf("%d", i);
+	
+	printf("정수 하나를 입력하시오 :");
+	scanf("%d", &i);
 	
 	if (i > 0)
 		printf("양수입니다.\n");
-	if (i < 0)
+	else if (i < 0)
 		printf("음수입니다.\n");
-	if (i = 0)
+	else
 		printf("0입니다.\n"); 
 		 
-	
 	return 0;
 }
