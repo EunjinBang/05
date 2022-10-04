a56b7b7cebf04f8d61fc1e827957f6b93b518025
EunjinBang <twin1963@sookmyung.ac.kr>
2022-10-04 17:05:36 +0900

    03

--- main.c
+++ main.c
@@ -5,20 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
+	char c;
+	int num=0;
 
-	printf("정수 하나를 입력하시오 :");
-	scanf("%d", &i);
-	
-	if (i > 0)
-		printf("절대값은 %d입니다.\n", i);
-	else if (i < 0)
+	printf("input a string :");
+
+	while ((c = getchar()) != '\n') //입력 문자가 개행문자가 나올 때까지 반복 
 	{
-		i = -i;
-		printf("절대값은 %d입니다.\n", i);
+		if (c >= '0' && c <= '9')
+			num = num+1;
 	}
-	else
-		printf("0입니다.\n"); 
-		 
+	printf("the number of digits is %d\n", num);
 	return 0;
 }
